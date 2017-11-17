3d72f469607e0653a5ecf78dd56f050b6f5d10f7
cloudy <im@joe.cloud>
2017-11-17 02:54:31 -0600

    Base test with set one: resistors

---
+++ descriptive_analysis/descriptive_analysis.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+#AUTHOR: Joe Cloud
+#PURPOSE: Perform simple descriptive analysis for Probability & Statistics for Engineers, project
+#UTA FALL 2017
+
+
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+
+
+DATA_FILE = "../set_one/resistor_vals.csv"  # Set to default list
+
+QUARTILES = [25, 50, 75]
+
+if len(sys.argv) > 1:
+    DATA_FILE = sys.argv[1]
+
+
+def main():
+    sample_vals = np.genfromtxt(DATA_FILE, delimiter=',')
+    print(sample_vals)
+    
+    sample_mean = np.mean(sample_vals)
+    
+    print(sample_mean)
+
+    sample_std = np.std(sample_vals)
+
+    print(sample_std)
+    
+    # Calculate quartiles
+    sample_quarts = []
+    for quart in QUARTILES:
+        sample_quarts.append(np.percentile(sample_vals, quart))
+
+    
+    print(sample_quarts)
+    
+    # Construct box-and-whisker plot, a.k.a. boxplot
+    fig = plt.figure()
+    ax = plt.subplot(111)
+    ax.boxplot(sample_vals, 0, 'kp', 0)
+    
+    fig.savefig('plot.png')
+
+
+
+
+if __name__ == "__main__":
+    main()
